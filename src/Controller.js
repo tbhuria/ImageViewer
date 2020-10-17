fe88590f7e3251f3de2a84b77b7082338efad252
Tarun <tbhuria@gmail.com>
2020-10-17 22:31:52 +0530

    Login page created

--- src/Controller.js
+++ src/Controller.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import Login from './screens/login/Login';
 import Home from './screens/home/Home';
-import Header from './common/header/Header';
+import { BrowserRouter as Router} from 'react-router-dom';
 
-class Controller extends Component{
-    render(){
-        return(
-                <div>
-                    <Header/>
-                </div>
+import { Route, Redirect } from 'react-router'
 
-        )
-    }
+
+class Controller extends Component {
+
+  constructor() {
+    super();
+    this.baseUrl = "v1/users/self/";
+  }
+  render() {
+    console.log("LoggedIn value is :" +this.props.loggedIn);
+    return (
+      <Router>
+        <div className="main-container">
+          <Route exact path='/' render={(props) => <Login {...props} baseUrl={this.baseUrl} />} />
+          <Route path='/home' render={(props) => (sessionStorage.getItem("access-token") !==null? <Home {...props} baseUrl={this.baseUrl} loggedIn="true" showSearchTab ="true" /> :  <Redirect to="/"/>)} />
+         </div>
+      </Router>
+    )
+  }
 }
+
 export default Controller;
